39bb7d2116567ec4d287e69d0ba54a4110ba3830
yassine4real <yssnabbou2002@gmail.com>
2024-12-25 21:53:59 +0100

    first commit

---
+++ src/test/java/com/stms/smarttaskmanagersystem/SmartTaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stms.smarttaskmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
